ee9378bfde9f015c9de9d4a5b979a31e45bf1b90
shashankpantiitbhilai <shashankpant94115@gmail.com>
2024-06-24 22:32:17 +0530

    Portal for format convertor

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const fileRoutes = require('./routes/fileRoutes');
+const fileRoutes = require('./routes/fileroutes');
 const path = require('path');
 const app = express();
 const PORT = 5000;
-
-app.use(cors());
+const origin =
+    process.env.NODE_ENV === "production"
+        ? "https://formatconvertor.vercel.app"
+        : "http://localhost:5000";
+app.use(cors({
+    origin,
+    methods: ['GET', 'POST', 'OPTIONS', 'PUT', 'PATCH', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
+}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/output', express.static(path.join(__dirname, 'output')));
